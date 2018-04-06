44bc09db4d5441f11102ab6a4f7aecca7dbefa36
Jérôme Desmares <jdesmare@student.42.fr>
2018-04-06 19:52:18 +0200

    Added Patient and Professional Profiles (#5)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,19 +1,51 @@
-from api.models import User, PatientProfile
+from api.models import User, PatientProfile, ProfessionalProfile, Report
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email', 'profile')
+        fields = ('is_active', 'email', 'profile', 'created_at', 'updated_at')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-
         
 class PatientProfileSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(
+        many=False,
+        read_only=True,
+        view_name='user-detail'
+    )
+
     class Meta:
         model = PatientProfile
-        fields = ('user', 'vital_card_number')
+        fields = ('user', 'first_name', 'last_name', 'birth_date', 'vital_card_number', 'created_at', 'updated_at')
+
+class ProfessionalProfileSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(
+        many=False,
+        read_only=True,
+        view_name='user-detail'
+    )
+
+    class Meta:
+        model = ProfessionalProfile
+        fields = ('user', 'first_name', 'last_name', 'job', 'created_at', 'updated_at')
+
+class ReportSerializer(serializers.HyperlinkedModelSerializer):
+    patient = serializers.HyperlinkedRelatedField(
+        many=False,
+        read_only=True,
+        view_name='patientprofile-detail'
+    )
+    professional = serializers.HyperlinkedRelatedField(
+        many=False,
+        read_only=True,
+        view_name='professionalprofile-detail'
+    )
+
+    class Meta:
+        model = Report
+        fields = ('patient', 'professional', 'content', 'created_at', 'updated_at')
