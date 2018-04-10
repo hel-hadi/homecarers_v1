a057a2d996735dcbb2c0f98be9159b1cc2d86077
futureunicorne <elhadi-hamza@hotmail.fr>
2018-04-10 22:49:47 +0200

    Beta front

--- front/src/index.js
+++ front/src/index.js
@@ -1,38 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
 import { Sidebar } from 'react-responsive-sidebar'
 import 'semantic-ui-css/semantic.min.css'
 import { BrowserRouter, Route, HashRouter } from 'react-router-dom'
-import decode from 'jwt-decode'
-import rootReducer from './rootReducer.jsx'
 import App from './App.jsx'
-import {userLoggedIn} from './actions/auth.jsx'
 
-const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
-);
-
-if (localStorage.homecareJWT) {
-    const payload = decode(localStorage.homecareJWT);
-    const user =  {
-        token: localStorage.homecareJWT,
-        email: payload.email,
-        confirmed: payload.confirmed
-    };
-    store.dispatch(userLoggedIn(user));
-}
 
 ReactDOM.render(
   <BrowserRouter>
-      <Provider store={store}>
           <HashRouter>
             <Route component={App} />
           </HashRouter>
-      </Provider>
   </BrowserRouter>,
   document.getElementById('app')
 );
