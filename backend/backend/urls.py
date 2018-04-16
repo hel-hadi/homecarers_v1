d5c03c07a311fc1b76b9635a236faac76e913b37
Jérôme Desmares <jdesmare@student.42.fr>
2018-04-16 19:22:43 +0200

    routes fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,16 +4,16 @@ from rest_framework import routers
 from api import views
 
 router = routers.DefaultRouter()
-router.register(r'api/landing_users', views.LandingUserViewSet)
-router.register(r'api/users', views.UserViewSet)
-router.register(r'api/patients', views.PatientProfileViewSet)
-router.register(r'api/pros', views.ProfessionalProfileViewSet)
-router.register(r'api/reports', views.ReportViewSet)
+router.register(r'landing_users', views.LandingUserViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'patients', views.PatientProfileViewSet)
+router.register(r'pros', views.ProfessionalProfileViewSet)
+router.register(r'reports', views.ReportViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^', admin.site.urls),
+    url(r'api/', include(router.urls)),
+    url(r'admin', admin.site.urls),
     url(r'^', include('rest_framework.urls', namespace='rest_framework'))
 ]
