9e51bfaa9b75d3abf43567eedb119e78a68d1d2a
Jérôme Desmares <jdesmare@student.42.fr>
2018-05-05 15:13:56 +0200

    test for landing

--- backend/api/tests/test_landing_user.py
+++ backend/api/tests/test_landing_user.py
@@ -1,16 +1,22 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from api.models import LandingUser, ContactMessage
 
 class LandingUserTests(APITestCase):
-    client = APIClient()    
-    
-    def register_mail(self):
-        url = reverse('api:landing_users')
-        data = {'email': 'toto@gmail.com', 'code_postal': '75000' }
+    def test_register_mail(self):
+        url = '/api/landing_users/'
+        data = {'email': 'lolilol@gmail.com', 'code_postal': '75000' }
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data, data)
+        self.assertEqual(LandingUser.objects.count(), 1)
+        self.assertEqual(LandingUser.objects.get().email, 'lolilol@gmail.com')
 
-landing_user = LandingUserTests()
-landing_user.register_mail()
+class ContactMessageTests(APITestCase):
+    def test_contact(self):
+        url = '/api/contact_messages/'
+        data = {'email': 'lolilol@gmail.com', 'message': 'I can do all things' }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ContactMessage.objects.count(), 1)
+        self.assertEqual(ContactMessage.objects.get().email, 'lolilol@gmail.com')
