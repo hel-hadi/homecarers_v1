9e46f50408ec62f44530ed6a3d9780d85ed18abf
futureunicorne <elhadi-hamza@hotmail.fr>
2018-05-21 19:58:49 +0200

    finish

--- front/src/components/forms/BetaForm.jsx
+++ front/src/components/forms/BetaForm.jsx
@@ -14,7 +14,7 @@ class BetaForm extends React.Component {
             count_global: 1,
             data: {
                 email: '',
-                code_postal: ''
+                postal_code: ''
             },
             loading: this.props.loader,
             errors: {},
@@ -32,8 +32,8 @@ class BetaForm extends React.Component {
         this.setState({ count_global: 1 });
         const errors = this.validate(this.state.data);
         this.setState({ errors });
-        if (!this.state.data.code_postal){
-            this.setState({ code_postal: '78000' });
+        if (!this.state.data.postal_code){
+            this.setState({ postal_code: '78000' });
         }
         if (Object.keys(errors).length === 0) {
             this.setState({loading: true});
@@ -47,14 +47,14 @@ class BetaForm extends React.Component {
 
     onLoader() {
         this.state.errors.email = '';
-        this.state.errors.code_postal = '';
+        this.state.errors.postal_code = '';
         this.state.errors.global = null ;
     }
 
     validate = (data) => {
         const errors = {};
         if (!validator.isEmail(data.email)) errors.email = "Vous devez renseigner votre adresse mail !";
-        // if (!validator.isPostalCode(data.code_postal, 'FR')) errors.code_postal = "Le code postal est incorrect";
+        // if (!validator.isPostalCode(data.postal_code, 'FR')) errors.postal_code = "Le code postal est incorrect";
         return errors;
     };
 
@@ -95,7 +95,7 @@ class BetaForm extends React.Component {
                                             <br/>
                                             Nous utiliserons votre adresse e-mail pour vous envoyer occasionnellement
                                             des informations concernant le développement et le déroulement
-                                            de la bêta de Homecarers.
+                                            de la bêta de homecarers.
                                             <br/><br/>
                                             Votre code postal sera utilisé uniquement à des fins statistiques par homecarers et
                                             ne sera pas transmis à des tiers.
@@ -103,11 +103,11 @@ class BetaForm extends React.Component {
                                         </div>
                                         <input
                                             className="beta"
-                                            type="code_postal"
-                                            id="code_postal"
-                                            name="code_postal"
+                                            type="postal_code"
+                                            id="postal_code"
+                                            name="postal_code"
                                             placeholder="Code postal"
-                                            value={data.code_postal}
+                                            value={data.postal_code}
                                             onChange={this.onChange}
                                         />
                                         <br/>
@@ -126,7 +126,7 @@ class BetaForm extends React.Component {
                         {(count_global === 1 && errors.email) && <InlineError text={errors.email}/>}
 
                         {(count_global === 1 && errors.global) && <InlineError text={errors.global} />}
-                        {(errors.email || errors.global || errors.code_postal) && this.onLoader()}
+                        {(errors.email || errors.global) && this.onLoader()}
 
                     </Form>
                     :
@@ -162,7 +162,7 @@ class BetaForm extends React.Component {
                                                 <br/>
                                                 Nous utiliserons votre adresse e-mail pour vous envoyer occasionnellement
                                                 des informations concernant le développement et le déroulement
-                                                de la bêta de Homecarers.
+                                                de la bêta de homecarers.
                                                 <br/><br/>
                                                 Votre code postal sera utilisé uniquement à des fins statistiques par homecarers et
                                                 ne sera pas transmis à des tiers.
@@ -170,11 +170,11 @@ class BetaForm extends React.Component {
                                             </div>
                                             <input
                                                 className="beta2"
-                                                type="code_postal"
-                                                id="code_postal"
-                                                name="code_postal"
+                                                type="postal_code"
+                                                id="postal_code"
+                                                name="postal_code"
                                                 placeholder="Code postal"
-                                                value={data.code_postal}
+                                                value={data.postal_code}
                                                 onChange={this.onChange}
                                             />
                                             <br/>
