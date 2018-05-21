b266f0a8d4e90489b079d7c4e1eceb0e66b498db
Jérôme Desmares <jdesmare@student.42.fr>
2018-05-21 20:19:50 +0200

    Landing improvements

--- front/src/components/forms/BetaForm.jsx
+++ front/src/components/forms/BetaForm.jsx
@@ -4,6 +4,8 @@ import 'semantic-ui-css/semantic.min.css'
 import PropTypes from 'prop-types';
 import validator from 'validator';
 import { InlineError } from '../../actions/routeSplit'
+import Popup from "reactjs-popup"
+import '@css/styles.css'
 
 class BetaForm extends React.Component {
     constructor(props) {
@@ -12,7 +14,7 @@ class BetaForm extends React.Component {
             count_global: 1,
             data: {
                 email: '',
-                code_postal: '78520'
+                postal_code: ''
             },
             loading: this.props.loader,
             errors: {},
@@ -30,6 +32,9 @@ class BetaForm extends React.Component {
         this.setState({ count_global: 1 });
         const errors = this.validate(this.state.data);
         this.setState({ errors });
+        if (!this.state.data.postal_code){
+            this.setState({ postal_code: '78000' });
+        }
         if (Object.keys(errors).length === 0) {
             this.setState({loading: true});
             this.props
@@ -42,13 +47,14 @@ class BetaForm extends React.Component {
 
     onLoader() {
         this.state.errors.email = '';
+        this.state.errors.postal_code = '';
         this.state.errors.global = null ;
     }
 
     validate = (data) => {
         const errors = {};
         if (!validator.isEmail(data.email)) errors.email = "Vous devez renseigner votre adresse mail !";
-        // if (!validator.isPostalCode(data.code_postal, 'FR')) errors.code_postal = "Le code postal est incorrect";
+        // if (!validator.isPostalCode(data.postal_code, 'FR')) errors.postal_code = "Le code postal est incorrect";
         return errors;
     };
 
@@ -59,7 +65,7 @@ class BetaForm extends React.Component {
         return (
             <div>
                 {!this.props.active ?
-                    <Form onSubmit={this.onSubmit} loading={loading} className="ui form">
+                    <Form loading={loading} className="ui form">
                         <div className="ui centered grid">
                             <div className="ui huge icon input">
                                 <input className="beta"
@@ -71,31 +77,60 @@ class BetaForm extends React.Component {
                                        onChange={this.onChange}
                                 />
                             </div>
-                            <button className="ui large button clear betabutt">
+                            <Popup
+                                trigger={
+                                <button className="ui large button clear betabutt">
                                     <span className="light1">
                                         S'inscrire à la bêta
                                     </span>
-                            </button>
+                                </button>
+                                } modal>
+                                {close => (
+                                    <div className="modal">
+                                        <a className="close" onClick={close}>
+                                            &times;
+                                        </a>
+                                        <div className="header"> Confirmation </div>
+                                        <div className="content1">
+                                            <br/>
+                                            Nous utiliserons votre adresse e-mail pour vous envoyer occasionnellement
+                                            des informations concernant le développement et le déroulement
+                                            de la bêta de Homecarers.
+                                            <br/><br/>
+                                            Votre code postal sera utilisé uniquement à des fins statistiques par Homecarers et
+                                            ne sera pas transmis à des tiers.
+                                            <br /><br />
+                                        </div>
+                                        <input
+                                            className="beta"
+                                            type="postal_code"
+                                            id="postal_code"
+                                            name="postal_code"
+                                            placeholder="Code postal"
+                                            value={data.postal_code}
+                                            onChange={this.onChange}
+                                        />
+                                        <br/>
+                                        <div className="actions">
+                                            <button  className="positive ui button"
+                                                     onClick={() => {this.onSubmit(); close()}}>Soumettre</button>
+                                            <button className="ui button" onClick={() => {close() }}>
+                                                Fermer
+                                            </button>
+                                        </div>
+                                    </div>
+                                )}
+                            </Popup>
                         </div>
-                        {/*<div className="ui input large focus" error={!!errors.code_postal}>*/}
-                        {/*<input*/}
-                        {/*type="code_postal"*/}
-                        {/*id="code_postal"*/}
-                        {/*name="code_postal"*/}
-                        {/*placeholder="Code Postal"*/}
-                        {/*value={data.code_postal}*/}
-                        {/*onChange={this.onChange}*/}
-                        {/*/>*/}
-                        {/*</div>*/}
-                        {/*{errors.code_postal && <InlineError text={errors.code_postal} />}*/}
 
                         {(count_global === 1 && errors.email) && <InlineError text={errors.email}/>}
+
                         {(count_global === 1 && errors.global) && <InlineError text={errors.global} />}
                         {(errors.email || errors.global) && this.onLoader()}
 
                     </Form>
                     :
-                    <Form onSubmit={this.onSubmit} loading={loading} className="ui centered form">
+                    <Form loading={loading} className="ui centered form">
                         <div className="ui centered grid">
                             <div className="ui huge icon input">
                                 <input className="ui centered beta1"
@@ -109,11 +144,51 @@ class BetaForm extends React.Component {
                             </div>
                         </div>
                             <div className="ui centered grid">
-                            <button className="ui large button centered betabutt1">
+                                <Popup
+                                    trigger={
+                                        <button className="ui large button clear betabutt1">
                                     <span className="light1">
                                         S'inscrire à la bêta
                                     </span>
-                            </button>
+                                        </button>
+                                    } modal>
+                                    {close => (
+                                        <div className="modal">
+                                            <a className="close" onClick={close}>
+                                                &times;
+                                            </a>
+                                            <div className="header"> Confirmation </div>
+                                            <div className="content1">
+                                                <br/>
+                                                Nous utiliserons votre adresse e-mail pour vous envoyer occasionnellement
+                                                des informations concernant le développement et le déroulement
+                                                de la bêta de Homecarers.
+                                                <br/><br/>
+                                                Votre code postal sera utilisé uniquement à des fins statistiques par Homecarers et
+                                                ne sera pas transmis à des tiers.
+                                                <br /><br />
+                                            </div>
+                                            <input
+                                                className="beta2"
+                                                type="postal_code"
+                                                id="postal_code"
+                                                name="postal_code"
+                                                placeholder="Code postal"
+                                                value={data.postal_code}
+                                                onChange={this.onChange}
+                                            />
+                                            <br/>
+                                            <div className="actions">
+                                                <button  className="positive ui button"
+                                                         onClick={() => {this.onSubmit(); close()}}>Soumettre</button>
+                                                <button className="ui button" onClick={() => {close() }}>
+                                                    Fermer
+                                                </button>
+                                            </div>
+                                        </div>
+                                    )}
+                                </Popup>
+
                             </div>
                         {(count_global === 1 && errors.email) && <InlineError text={errors.email}/>}
                         {(count_global === 1 && errors.global) && <InlineError text={errors.global} />}
