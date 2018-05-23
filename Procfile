22cdba4766bfac787ae31c223e89dff863673327
Jérôme Desmares <jdesmare@student.42.fr>
2018-05-23 11:08:01 +0200

    hack

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: honcho -f ProcfileHoncho start
+web: npm start
--- front/src/api.jsx
+++ front/src/api.jsx
@@ -3,19 +3,19 @@ import axios from 'axios'
 export default {
     user: {
         beta: (data) =>
-            axios.post('http://0.0.0.0:8000/api/landing_users/', data,
+            axios.post('http://prod-homecarers-api.herokuapp.com/api/landing_users/', data,
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         contact: (data) =>
-            axios.post('http://0.0.0.0:8000/api/contact_messages/', data,
+            axios.post('http://prod-homecarers-api.herokuapp.com/api/contact_messages/', data,
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         login: (credentials) =>
-            axios.post('http://0.0.0.0:8000/api/auth', { credentials },
+            axios.post('https://prod-homecarers-api.herokuapp.com/api/auth', { credentials },
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         signup: user =>
-            axios.post('http://0.0.0.0:8000/api/users', { user },
+            axios.post('https://prod-homecarers-api.herokuapp.com/api/users', { user },
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         confirm: token =>
-            axios.post('http://0.0.0.0:8000/api/auth/confirmation', { token },
+            axios.post('https://prod-homecarers-api.herokuapp.com/api/auth/confirmation', { token },
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user)
     },
 }
