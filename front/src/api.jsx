7e69f1d8e7afc6b050c721811bc5d55ddaf1c140
Jérôme Desmares <jdesmare@student.42.fr>
2018-05-23 10:32:49 +0200

    api fail

--- front/src/api.jsx
+++ front/src/api.jsx
@@ -3,19 +3,19 @@ import axios from 'axios'
 export default {
     user: {
         beta: (data) =>
-            axios.post('http://www.homecarers.co/api/landing_users/', data,
+            axios.post('http://0.0.0.0:8000/api/landing_users/', data,
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         contact: (data) =>
-            axios.post('http://www.homecarers.co/api/contact_messages/', data,
+            axios.post('http://0.0.0.0:8000/api/contact_messages/', data,
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         login: (credentials) =>
-            axios.post('https://www.homecarers.co/api/auth', { credentials },
+            axios.post('http://0.0.0.0:8000/api/auth', { credentials },
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         signup: user =>
-            axios.post('https://www.homecarers.co/api/users', { user },
+            axios.post('http://0.0.0.0:8000/api/users', { user },
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user),
         confirm: token =>
-            axios.post('https://www.homecarers.co/api/auth/confirmation', { token },
+            axios.post('http://0.0.0.0:8000/api/auth/confirmation', { token },
                 { headers: {'Content-Type': 'application/json',}}).then(res => res.data.user)
     },
 }
